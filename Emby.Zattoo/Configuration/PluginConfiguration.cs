6e83330ed9477a6f206e3ebcf17db92691450788
freezy <freezy@kodi.tv>
2017-03-15 22:37:37 +0100

    Added config page.

--- Emby.Zattoo/Configuration/PluginConfiguration.cs
+++ Emby.Zattoo/Configuration/PluginConfiguration.cs
@@ -2,7 +2,25 @@
 
 namespace Emby.Zattoo.Configuration
 {
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-    }
+	public class PluginConfiguration : BasePluginConfiguration
+	{
+		public string ZapiAuthUrl { get; set; }
+		public string ZapiUrl { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public bool Swiss { get; set; }
+		public int MaxBandwidth { get; set; }
+		public string AudioStream { get; set; }
+
+		public PluginConfiguration()
+		{
+			ZapiAuthUrl = "https://zattoo.com";
+			ZapiUrl = "http://zattoo.com";
+			Username = "";
+			Password = "";
+			Swiss = true;
+			MaxBandwidth = 5000;
+			AudioStream = "B";
+		}
+	}
 }
--- Emby.Zattoo/Plugin.cs
+++ Emby.Zattoo/Plugin.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 using Emby.Zattoo.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
 namespace Emby.Zattoo
 {
-	public class Plugin : BasePlugin<PluginConfiguration>
+	public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 	{
 		public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
 		{
@@ -19,5 +20,15 @@ namespace Emby.Zattoo
 
 		public override string Name { get; } = "Zattoo";
 		public override string Description { get; } = "Provides live TV using Zattoo as a back-end.";
+
+		public IEnumerable<PluginPageInfo> GetPages()
+		{
+			 return new[] {
+                new PluginPageInfo {
+                    Name = Name,
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                }
+            };
+		}
 	}
 }
